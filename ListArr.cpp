033d2f2ccaaa59d684b3fff402bd38c45fdfb3b2
Basito0 <76927670+Basito0@users.noreply.github.com>
2023-04-06 16:00:47 -0400

    Add files via upload

---
+++ ListArr.cpp
@@ -0,0 +1,26 @@
+#include "ListArr.h"
+#include <stdio.h>
+
+int ListArr::size(){
+	cout<<this->tamano<<endl;
+}
+
+void ListArr::insert_left(int v){
+
+}
+
+void ListArr::insert_right(int v){
+
+}
+
+void ListArr::insert(int v, int i){
+
+}
+
+void ListArr::print(){
+
+}
+
+bool ListArr::find(){
+
+}
