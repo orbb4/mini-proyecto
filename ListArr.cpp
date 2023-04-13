6208f35e4f7a73821ff2e0d1d260267f57dd242c
orbb4 <114522263+orbb4@users.noreply.github.com>
2023-04-13 15:13:10 -0400

    Implementación del  insert_right (incompleto)

--- ListArr.cpp
+++ ListArr.cpp
@@ -12,13 +12,30 @@ void ListArr::insert_left(int v){
 	if(count = 0){
 		pNode first = new Node(b);
 		first.arr[0] = v;
+		head = first;
+		tail = first;
 	}
 }
-
+//ToDo: añadir nodos resumen por cada dos nodos ListArr
 void ListArr::insert_right(int v){
+	// si no hay ningun array creado
 	if(count = 0){
-		pNode first = new Node(b);
-		first.arr[0] = v;
+		pNode *first = new pNode(nullptr, b);
+		first->arr[0] = v;
+		first->numDeDatos++;
+		nodoHead = first;
+		nodoTail = first;
+	// si el arreglo de la derecha está lleno, se crea uno nuevo a la derecha de la cola
+	}else if(nodoTail->numDeDatos == b){
+		pNode *newNode = new pNode(nullptr, b);
+		newNode[0] = v;
+		newNode->numDeDatos++;
+		nodoTail->setPtrder(newNode);
+	// si queda espacio en la cola	
+	}else{
+		int posicion = (nodoTail->numDeDatos)-1
+		nodoTail->arr[pos]=v;
+		nodoTail->numDeDatos++;
 	}
 }
 
